978f6720cb4937e5f91a831fd2c0970d6e3910e1
ace34TT <tafinasoa35@gmail.com>
2023-01-29 01:42:29 +0300

    modif

--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -1,12 +1,16 @@
 import { CustomerModel } from "../schemas/customer.schema.js";
 
 export const insertCustomerMdl = async (customer) => {
-  CustomerModel.create(customer, function (err, small) {
-    if (err) {
-      console.log(err);
-    }
-    console.log("customer registered");
-  });
+  try {
+    CustomerModel.create(customer, function (err, small) {
+      if (err) {
+        console.log(err);
+      }
+      console.log("customer registered");
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 export const findCustomerByEmailMdl = async (email) => {
