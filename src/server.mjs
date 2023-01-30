0868e9297cbd1ffb09ba7cf5a65b614fe086d3b1
ace34TT <tafinasoa35@gmail.com>
2023-01-30 20:45:28 +0300

    port

--- src/server.mjs
+++ src/server.mjs
@@ -6,6 +6,6 @@ import config from "config";
 const port = config.get("server.port");
 
 const server = http.createServer(app);
-server.listen(0, () => {
-  console.log(`Server started on port: ${server.address().port}`);
+server.listen(port, () => {
+  console.log(`Server started on port: ${port}`);
 });
